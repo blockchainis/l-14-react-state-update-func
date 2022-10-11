c62ea7cd7f3b57761e59fc6c8975bf6ad31eaf53
hoon2 <sanghoon.engineer@gmail.com>
2022-10-12 07:42:31 +0900

    초기코드

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,9 +1,14 @@
 import { useState } from "react";
 function Counter() {
   const [number, setNumber] = useState(0);
+  function handlePlusThreeClick() {
+    setNumber(number + 3);
+  }
+
   function handlePlusClick() {
     setNumber(number + 1);
   }
+
   function handleMinusClick() {
     setNumber(number - 1);
   }
@@ -11,6 +16,7 @@ function Counter() {
   return (
     <>
       <h1> {number} </h1>
+      <button onClick={handlePlusThreeClick}> +3 </button>
       <button onClick={handlePlusClick}> + </button>
       <button onClick={handleMinusClick}> - </button>
     </>
